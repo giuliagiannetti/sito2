1b310da5b3febb7dcdabcc17b21439463c4701f9
Lorenzo Botti <bottilorenzobg@gmail.com>
2022-07-14 21:25:20 +0200

    da onclick a event listener

--- script.js
+++ script.js
@@ -3,13 +3,15 @@
 let btn = document.getElementById("stylebtn"); //restituisce un elemento che corrisponde al selettore CSS specificato
 let theme = document.getElementById("foglio-di-stile");// questo ti dice: nella funzione Javascript #foglio_di_stile si chiama theme
 
+btn.addEventListener('click', cambiastile)
+
 function cambiastile() {
     let stileref = theme.getAttribute("href");
     if (theme.getAttribute("href") == "light-mode.css") { 
-    theme.setAttribute ("href", "dark-mode.css") ;
+        theme.setAttribute ("href", "dark-mode.css") ;
     } else {
-    theme.setAttribute ("href", "light-mode.css");
+        theme.setAttribute ("href", "light-mode.css");
     }
 };
 
-btn.onclick = cambiastile;
+
