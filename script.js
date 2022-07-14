ded9856a6c80d238420c71e8f063342deec30d09
Lorenzo Botti <bottilorenzobg@gmail.com>
2022-07-14 21:31:34 +0200

    mini refactor di script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // PULSANTE LIGHT-DARK MODE
 
-let btn = document.getElementById("stylebtn"); //restituisce un elemento che corrisponde al selettore CSS specificato
-let theme = document.getElementById("foglio-di-stile");// questo ti dice: nella funzione Javascript #foglio_di_stile si chiama theme
+const button = document.getElementById("stylebtn"); //restituisce un elemento che corrisponde al selettore CSS specificato
+const theme = document.getElementById("foglio-di-stile");// questo ti dice: nella funzione Javascript #foglio_di_stile si chiama theme
 
-btn.addEventListener('click', cambiastile)
+console.log({ button })
+console.log({ element: document.getElementById("stylebtn") })
+
+button.addEventListener('click', cambiastile)
 
 function cambiastile() {
-    let stileref = theme.getAttribute("href");
     if (theme.getAttribute("href") == "light-mode.css") { 
         theme.setAttribute ("href", "dark-mode.css") ;
     } else {
         theme.setAttribute ("href", "light-mode.css");
     }
-};
-
-
+}
