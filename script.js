7bbd8be3e11c7f5f95036c9ba4177b4d6d252c45
giuliagiannetti <giulia.giannetti02@gmail.com>
2022-07-15 12:27:08 +0200

    boc shadow sull'header

--- script.js
+++ script.js
@@ -2,14 +2,31 @@
 
 let btn = document.getElementById("stylebtn"); //restituisce un elemento che corrisponde al selettore CSS specificato
 let theme = document.getElementById("foglio-di-stile");// questo ti dice: nella funzione Javascript #foglio_di_stile si chiama theme
+let cow = document.querySelector (".logo")
 
 function cambiastile() {
-    let stileref = theme.getAttribute("href");
     if (theme.getAttribute("href") == "light-mode.css") { 
     theme.setAttribute ("href", "dark-mode.css") ;
+    cow.setAttribute ("src", "img/muccainversa.png");
     } else {
     theme.setAttribute ("href", "light-mode.css");
+    cow.setAttribute ("src", "img/mucca1.png");
     }
 };
 
 btn.onclick = cambiastile;
+
+// MENU A TENDINA
+
+const menuBtn = document.querySelector('.menubtn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
