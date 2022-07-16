a67d62dce27bffb545458125b0df4b383b72fb43
JackTon01XD <108990676+JackTon01XD@users.noreply.github.com>
2022-07-16 13:14:10 +0200

    megaciao

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let btn = document.getElementById("stylebtn"); //restituisce un elemento che corrisponde al selettore CSS specificato
 let theme = document.getElementById("foglio-di-stile");// questo ti dice: nella funzione Javascript #foglio_di_stile si chiama theme
 let cow = document.querySelector (".logo")
-
+//Megaciao
 function cambiastile() {
     if (theme.getAttribute("href") == "light-mode.css") { 
     theme.setAttribute ("href", "dark-mode.css") ;
